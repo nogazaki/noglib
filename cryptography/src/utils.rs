a2c90b667e5c9276e8e0f586cbb0dd4d2dffcd37
Nguyên Vũ <38126172+nogazaki@users.noreply.github.com>
2024-06-13 04:27:17 +0000

    build!: use stable toolchain channel
    BREAKING CHANGE: drop cryptography crate

--- cryptography/src/utils.rs
+++
@@ -1,6 +0,0 @@
-//! Utilities module
-
-pub mod error_types;
-pub mod macros;
-pub mod traits;
-pub mod types;
--- cryptography/src/utils/error_types.rs
+++
@@ -1,5 +0,0 @@
-//! Define various error types for various operation
-
-/// Error type returns when output buffer is not large enough for data
-#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
-pub struct InsufficientMemoryError;
